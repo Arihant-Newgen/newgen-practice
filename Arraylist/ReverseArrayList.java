b8cf68d549c3d5e0f60448db83a695f4495c87a8
disha-newgen <128671028+disha-newgen@users.noreply.github.com>
2023-03-31 12:22:43 +0530

    Create ReverseArrayList.java

---
+++ Arraylist/ReverseArrayList.java
@@ -0,0 +1,21 @@
+package list;
+
+import java.util.ArrayList;
+
+public class ReverseArrayList 
+{
+	public static void main(String args[])
+	{
+		ArrayList<Integer>list=new ArrayList<>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(4);
+		list.add(5);
+		System.out.println("The list before reversing-: " + list);
+		for(int i=list.size()-1;i>=0;i--)
+		{
+			int val=list.get(i);
+			System.out.println(val);
+		}
+	}
