8d67c8d3eb766766f9d3ab194392905ff08f54c1
disha-newgen <128671028+disha-newgen@users.noreply.github.com>
2023-04-26 18:11:04 +0530

    Create Emp.java

---
+++ Inheritance2/Emp.java
@@ -0,0 +1,11 @@
+package inheritance;
+class Emp extends Person{
+	float salary;  
+	Emp(int id,String name,float salary){  
+	super(id,name);//reusing parent constructor  
+	this.salary=salary;  
+     }
+	void display()
+	{
+		System.out.println(id+" "+name+" "+salary);
+	}
