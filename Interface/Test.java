b040a44d9affad2e6fd43878568ff2b76dc026b9
disha-newgen <128671028+disha-newgen@users.noreply.github.com>
2023-04-30 23:15:14 +0530

    Create Test.java

---
+++ Interface/Test.java
@@ -0,0 +1,15 @@
+package interfaces;
+
+public class test {
+  public static void main()
+  {
+	  A obj=new B();
+	  obj.show();
+	  obj.config();
+	  X obj1=new B();
+	  obj1.run();
+	  obj1.interfacex();
+	  System.out.println(A.area);
+	  System.out.println(A.age);
+  }
+}
